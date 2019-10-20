a0f1fe402707f6066774e9b96d1e1b1d0fcfe613
Martin Boyanov <martin.boyanov@smule.com>
2019-10-20 22:46:36 +0300

    save user and model features

--- src/main/scala/GameRecommender.scala
+++ src/main/scala/GameRecommender.scala
@@ -7,6 +7,8 @@ object GameRecommender extends App{
 
   val cachedRatings = data.getTrainRatings.rdd.cache()
   val model = ALS.trainImplicit(cachedRatings, 50, 10)
+  model.userFeatures.repartition(1).saveAsTextFile("data_dir/user_features.txt")
+  model.productFeatures.repartition(1).saveAsTextFile("data_dir/game_features.txt")
   val ratings = ModelUtils.predict(data, model)
   val mrr = Metrics.mrr(data, ratings)
   print(s"MEAN RECIPROCAL RANK: $mrr")
