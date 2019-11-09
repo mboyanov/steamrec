f8415b3e26d3deea108e8a76a104200807b86ff6
Martin Boyanov <martin.boyanov@smule.com>
2019-11-09 15:43:53 +0200

    add notebooks

--- src/main/scala/com/steamrec/GameRecommender.scala
+++ src/main/scala/com/steamrec/GameRecommender.scala
@@ -6,9 +6,16 @@ object GameRecommender extends App{
   private lazy val logger: Logger = LoggerFactory.getLogger(GameRecommender.getClass)
   val ratingsFile = "data_dir/train-test-split-v2.json"
   val data = DataLoader.loadData(ratingsFile,8)
-  val ex = Experiment(10, 10, 0.01, 10)
   val cachedRatings = data.getTrainRatings.cache()
-  logger.info("Ratings loaded, cached")
-  ExperimentRunner.runExperiment(ex, cachedRatings, data)
+
+  val factors = Array(2, 5, 10, 20, 50, 100, 500)
+  val reg = Array(0, 1e-5, 1e-3, 1e-2, 1e-1, 1, 10)
+  val alphas = Array(1, 2, 5, 10, 20, 50, 100)
+  for (f<- factors; r <- reg; a <- alphas) {
+    val ex = Experiment(f, 10, r, a)
+    logger.info("Running experiment "+ex.toString)
+    ExperimentRunner.runExperiment(ex, cachedRatings, data)
+  }
+
 
 }
